eaf73eb8ecbf067c5a998fc4affd22c484c7942a
edoardo <registrazioni.edoardo@gmail.com>
2020-05-12 18:21:44 +0200

    un test

---
+++ src/test/java/it/edo/tests/test/CollectionTests.java
@@ -0,0 +1,17 @@
+package it.edo.tests.test;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class CollectionTests {
+
+    @Test
+    public void basicTest() {
+        log.info("ciao!!!");
+
+    }
+}
